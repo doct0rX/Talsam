bf047bc45ccf0d91522fec56ba7e4f038d305232
doct0rX <mustafaj4m@gmail.com>
2022-11-02 17:42:32 +0200

    finite state machine works now

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,13 @@
 
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <GOTStateMachine.h>
 
 
+GOTStateMachine stateMachine(50);	// execute every 50 milliseconds
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+#define BLINKING_LIGHT_RONDOMLY_INTERVAL 3000	// blinking light time
 
 const int pushButtonPin = 11;  // the number of the push button pin
 const int firstLed = 4;		// LEDs [D2.. D10]
@@ -17,46 +21,38 @@ const int seventhLed = 2;
 const int eighthLed = 7;
 const int ninthLed = 10;
 
+
 // TODO: remove this vars later and work with numbers directly
 int leds[9] = {firstLed, secondLed, thirdLed,
 			   forthLed, fifthLed, sixthLed,
 			   seventhLed, eighthLed, ninthLed};
 
-int pushButtonState = 0;
+bool pushButtonState = false;
 int publicMs = (millis() + 500)/1000;
 
 void initialize();
 void start();
-void randomLedsBlinking(unsigned long millisecondsInterval, unsigned long currentMs);
+void randomLedsBlinking();
+void setupStateMachine();
+void printOnScreenAndStopLeds();
 
 void setup() {
   	// initialize the Pins
 	initialize();
-  // digitalWrite(6, HIGH);
-  // digitalWrite(7, HIGH);
-  // digitalWrite(8, HIGH);
-  // digitalWrite(9, HIGH);
-  // digitalWrite(10, HIGH);
-
 	
 	lcd.setCursor(4,0);
 	lcd.print("Hackster");
+	
+	stateMachine.setStartState(randomLedsBlinking);
 }
 
 
 void loop() {
 	pushButtonState = digitalRead(pushButtonPin);
-	// digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-	// delay(1500);                       // wait for a second
-	// digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-	// delay(1500);
-
+	stateMachine.execute();
 	if (pushButtonState == HIGH) {
-		Serial.print("clicked");
-		publicMs = (millis() + 500)/1000 + 5;
+		stateMachine.setStartState(randomLedsBlinking);
 	}
-	randomLedsBlinking(5000, publicMs);
-	start();
 }
 
 
@@ -75,26 +71,23 @@ void initialize() {
 	lcd.clear();
 }
 
-void start() {
+
+void randomLedsBlinking() {
+	digitalWrite(leds[random(0, sizeof(leds)/sizeof(int))], HIGH);
+	delay(random(0, 200));
+	digitalWrite(leds[random(0, sizeof(leds)/sizeof(int))], LOW);
 	
+	if (stateMachine.isDelayComplete(BLINKING_LIGHT_RONDOMLY_INTERVAL)) {
+		stateMachine.changeState(printOnScreenAndStopLeds);
+		return;
+	}
 }
 
-
-/** making a state machine function to count the Leds seconds **/
-int liveMs = (millis() + 500) / 1000;
-// int ms = ((publicMs + millisecondsInterval) + 500) / 1000;
-
-void randomLedsBlinking(unsigned long millisecondsInterval, unsigned long currentMs) {
-	Serial.print("here ");
-	Serial.println(((publicMs + millisecondsInterval) + 500) / 1000);
-	if (liveMs < currentMs) {
-		digitalWrite(leds[random(0, sizeof(leds)/sizeof(int))], HIGH);
-		delay(random(0, 200));
-		digitalWrite(leds[random(0, sizeof(leds)/sizeof(int))], LOW);
-	} else {
-		for (size_t i = 0; i < sizeof(leds)/sizeof(int); i++) {
-			digitalWrite(leds[i], LOW);
-		}
+void printOnScreenAndStopLeds() {
+	lcd.clear();
+	lcd.setCursor(4,0);
+	lcd.print("DONE!!!");
+	for (size_t led = 0; led < sizeof(leds)/sizeof(int); led++) {
+		digitalWrite(leds[led], LOW);
 	}
-	Serial.println((millis()+500)/1000);
 }
